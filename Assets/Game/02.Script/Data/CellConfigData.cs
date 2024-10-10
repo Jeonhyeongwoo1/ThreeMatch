37580d475f082a86b1cc3f9d237d1eac9859b7e7
Jeonhyeongwoo1 <guddn1234k@gmail.com>
2024-10-11 02:11:37 +0900

    Update cell type

--- Assets/Game/02.Script/Data/CellConfigData.cs
+++ Assets/Game/02.Script/Data/CellConfigData.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ThreeMatch.InGame.Data
 {
@@ -8,7 +10,15 @@ namespace ThreeMatch.InGame.Data
     public class CellConfigData : ScriptableObject
     {
         public Sprite[] SpriteArray => _spriteArray;
+        public Sprite BombSprite => _bombSprite;
+        public Sprite VerticalRocketSprite => _verticalRocketSprite;
+        public Sprite HorizontalRocketSprite => _horizontalRocketSprite;
+        public Sprite WandSprite => _wandSprite;
         
         [SerializeField] private Sprite[] _spriteArray;
+        [SerializeField] private Sprite _bombSprite;
+        [SerializeField] private Sprite _verticalRocketSprite;
+        [SerializeField] private Sprite _horizontalRocketSprite;
+        [SerializeField] private Sprite _wandSprite;
     }
 }
--- Assets/Game/02.Script/Enum.cs
+++ Assets/Game/02.Script/Enum.cs
@@ -8,7 +8,11 @@ public enum BlockType
 
 public enum CellType
 {
-    
+    None,
+    Normal, //일반형
+    Rocket,
+    Wand,
+    Bomb,
 }
 
 public enum CellImageType
@@ -19,4 +23,22 @@ public enum CellImageType
     Pink,
     Purple,
     Red
+}
+
+public enum CellMatchedType
+{
+    None = 0,
+    Three = 3,
+    Four = 4, //로켓
+    Five = 5,
+    Five_OneLine = Five, // 완드
+    Five_Shape, // 폭탄 (십자가, T, L 모양일 경우)
+    Vertical_Four,
+    Horizontal_Four, 
+}
+
+public enum CellCombinationType
+{
+    None = 0,
+    
 }
